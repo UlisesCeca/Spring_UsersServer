8bf1d7a0ab20157a0d03a49a02109e7463b38b0e
UlisesCeca <ulises@ulisesceca.com>
2018-08-09 16:38:38 +0200

    first commit

---
+++ src/main/java/com/example/ulises/template/TemplateApplication.java
@@ -0,0 +1,22 @@
+package com.example.ulises.template;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+
+	@Bean
+	public ResourceConfig jerseyConfig() {
+		final ResourceConfig resourceConfig = new ResourceConfig();
+
+		return resourceConfig;
+	}
+}
